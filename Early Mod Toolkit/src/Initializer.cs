63e88ca362c5525859ece371e179728431d4dd5b
Zach K <huntingmanzach@gmail.com>
2023-08-02 23:26:54 -0400

    Bump version

--- Early Mod Toolkit/src/Initializer.cs
+++ Early Mod Toolkit/src/Initializer.cs
@@ -65,6 +65,7 @@ namespace EMTK {
                 Path.Combine(basepath, "assets")
             });
 
+            GamePaths.DataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "VintagestoryData");
             modProfilePath = Path.Combine(GamePaths.DataPath, "ModProfiles");
             activeModProfilePath = Path.Combine(modProfilePath, "ActiveProfile");
             
